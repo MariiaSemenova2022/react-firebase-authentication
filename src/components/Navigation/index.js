bf445a54745b545cb041b8bb826393e668ac33f4
MariiaSemenova2022 <semenova.mariia@gmail.com>
2022-10-23 12:48:29 -0500

    Add Admin link to Navigation component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes'
 
-const Navigation = () => (
+import SignOutButton from '../SignOut';
+
+const Navigation = ({authUser}) => (
   <div>
-    <h1>Navigation</h1>
+    {authUser != null ? <NavigationAuth/> : <NavigationNonAuth/>}
   </div>
 );
 
+const NavigationAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>Home</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ACCOUNT}>Account</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ADMIN}>Admin</Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+)
+
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+  </ul>
+)
+
+const withNavigate = Component => props => {
+  const navigate = useNavigate();
+  return <Component {...props} navigate={navigate} />
+}
+
 export default Navigation;
+
+export { withNavigate };
